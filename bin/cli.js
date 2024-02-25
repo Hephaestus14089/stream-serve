6932b612832feeedea15220f1a1a0b1d8d1d4d0d
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2024-02-26 00:08:35 +0530

    help option

--- bin/cli.js
+++ bin/cli.js
@@ -18,6 +18,19 @@ const settings = {
   file_path: null
 };
 
+const displayHelp = () => {
+  console.log(`
+    Usage: \n\tstream-serve [options] [file-path]
+  
+    Description: \n\tServes stream of video files.
+
+    Options: \n
+    \t--help, -h\tshow this help message and exit
+    \t--port, -p\tspecify the port to run the stream server on (default: 80)
+    \t--chunk-size. -c\tspecify the size of each chunk in the stream (default: 200kb)
+  `);
+};
+
 const argsParse = (args) => {
   console.log(args);  // debug output
 
@@ -27,30 +40,36 @@ const argsParse = (args) => {
     'file_path': false
   };
 
-  for (let i = 2; i < args; i++) {
-    if (['--help', 'h'].includes(args[i])) {
-      // display help
-      process.exit(0);
+  for (let i = 2; i < args.length; i++) {
+    if (args[i].charAt(0) === '-') {  // is an option
+      if (['--help', '-h'].includes(args[i])) {
+        displayHelp();
+        process.exit(0);
+      }
+      else if (['--chunk-size', '-c'].includes(args[i])) {
+        // if (isOptionSet['chunk-size'])
+        //   badCommand()
+        //
+        // isOptionSet['chunk-size'] = true;
+        //
+        // const chunkSize = args[i + 1]
+        // validate chunk size value
+        // set chunk size
+      }
+      else if (['--port', '-p'].includes(args[i])) {
+        // check if already set 
+        // 
+        // validate value
+        // set port
+      }
+      else {
+        // bad command error
+        // display help command
+      }
     }
-    else if (['--chunk-size', '-c'].includes(args[i])) {
-      // if (isOptionSet['chunk-size'])
-      //   badCommand()
-      //
-      // isOptionSet['chunk-size'] = true;
-      //
-      // const chunkSize = args[i + 1]
-      // validate chunk size value
-      // set chunk size
-    }
-    else if (['--port', '-p'].includes(args[i])) {
-      // check if already set 
-      // 
-      // validate value
-      // set port
-    }
-    else {
+    else {  // is a file path
       // if (isOptionSet('file-path'))
-      //   badCommand()
+      //  bad file path error
       //
       // isOptionSet['file-path'] = true;
       // validate file path 
