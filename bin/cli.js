fe83f2e3db3137ef12e49e314ad860094d31ad17
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2024-02-26 00:44:08 +0530

    chunk-size option

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,3 @@
-/**
- * Options :-
- * --chunk-size, -c
- * --port, -p
- * --help, -h
- */
-
-/**
- * Flow of the app algorithm :-
- * get user input (command)
- * verify user input
- * do/run stuff accordingly
- */
-
 const settings = {
   chunk_size: 204800, // default chunk size: 200kb
   port: 80, // default port
@@ -31,8 +17,16 @@ const displayHelp = () => {
   `);
 };
 
+const displayErrMsg = (msg) => {
+  console.log(`err: ${msg}`);
+};
+
+const badCommand = () => {
+  displayErrMsg("Unable to understand. Please recheck command syntax.");
+};
+
 const argsParse = (args) => {
-  console.log(args);  // debug output
+  // console.log(args);  // debug output
 
   isOptionSet = {
     'chunk_size': false,
@@ -47,14 +41,25 @@ const argsParse = (args) => {
         process.exit(0);
       }
       else if (['--chunk-size', '-c'].includes(args[i])) {
-        // if (isOptionSet['chunk-size'])
-        //   badCommand()
-        //
-        // isOptionSet['chunk-size'] = true;
-        //
-        // const chunkSize = args[i + 1]
-        // validate chunk size value
-        // set chunk size
+        if (isOptionSet['chunk-size']) {
+          badCommand();
+          displayHelp();
+          process.exit(1);
+        }
+        
+        isOptionSet['chunk_size'] = true;
+        
+        if (args[i + 1] === undefined) {
+          displayErrMsg("You must provide a chunk size if you use the option.");
+          process.exit(1);
+        }
+        
+        if (isNaN(args[i + 1]) || Number(args[i + 1]) <= 0) {
+          displayErrMsg("Bad value.");
+          process.exit(1);
+        }
+
+        settings.chunk_size = Number(args[i + 1]);
       }
       else if (['--port', '-p'].includes(args[i])) {
         // check if already set 
@@ -67,7 +72,7 @@ const argsParse = (args) => {
         // display help command
       }
     }
-    else {  // is a file path
+    else if (args[i - 1].charAt(0) !== '-') {  // is a file path
       // if (isOptionSet('file-path'))
       //  bad file path error
       //
@@ -78,11 +83,20 @@ const argsParse = (args) => {
   }
 };
 
+/**
+ * Flow of the app algorithm :-
+ * get user input (command)
+ * verify user input
+ * do/run stuff accordingly
+ */
+
 function main() {
-  console.log(process.argv);  // debug output
-  console.log(settings);  // debug output
+  // console.log(process.argv);  // debug output
+  // console.log(settings);  // debug output
 
   argsParse(process.argv);
+
+  console.log(settings);  // debug output
 } // end of main
 
 main();
